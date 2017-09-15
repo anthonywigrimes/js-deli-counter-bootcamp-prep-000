ebfc07d5d982b881d7f3740573aa9db487a93a4e
Anthony Grimes <agrimes@charter.net>
2017-09-15 17:31:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = [];
 
-function takeNumber(katzDeliLine, name) {
+function takeANumber(katzDeliLine, name) {
     var index = katzDeliLine.push(name) + 1;
     return "You are number " + index + " in line.";
 };
